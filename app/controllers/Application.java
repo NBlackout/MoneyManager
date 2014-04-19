5ea121a54f5ba74fa87fa37492894a7de26dbd9a
NBlackout <NBlackout@hotmail.fr>
2014-04-19 12:39:37 +0200

    Random account generation with pretty currency printing

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,37 @@
 package controllers;
 
+import java.util.Calendar;
+import java.util.List;
+
+import models.Account;
+import models.Bank;
 import play.mvc.Controller;
 
 public class Application extends Controller {
 
 	public static void index() {
-		render();
+		Account.deleteAll();
+		Bank.deleteAll();
+
+		for (int i = 0; i < 3; i++) {
+			Bank bank = new Bank();
+			bank.label = "Banque " + i;
+			bank.save();
+		}
+
+		for (int i = 0; i < 10; i++) {
+			int index = (int) (Math.random() * Bank.count());
+
+			Account account = new Account();
+			account.bank = Bank.<Bank>findAll().get(index);
+			account.label = "Compte " + i;
+			account.balance = Math.random() * 10000;
+			account.lastSync = Calendar.getInstance().getTimeInMillis();
+			account.save();
+		}
+
+		List<Account> accounts = Account.findAll();
+
+		render(accounts);
 	}
 }
