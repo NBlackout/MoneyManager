dc6a5af37a7676d68f114eeb9f8f10d8d421a784
NBlackout <NBlackout@hotmail.fr>
2014-04-19 12:10:55 +0200

    Model relations added on entity

--- app/models/Account.java
+++ app/models/Account.java
@@ -1,6 +1,7 @@
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 import play.db.jpa.Model;
 
@@ -9,8 +10,10 @@ public class Account extends Model {
 
 	private static final long serialVersionUID = 8814425930298906224L;
 
+	@ManyToOne
 	public Customer customer;
 
+	@ManyToOne
 	public Bank bank;
 
 	public String label;
--- app/models/Transaction.java
+++ app/models/Transaction.java
@@ -1,6 +1,7 @@
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 import play.db.jpa.Model;
 
@@ -9,6 +10,7 @@ public class Transaction extends Model {
 
 	private static final long serialVersionUID = 2305214514018528308L;
 
+	@ManyToOne
 	public Account account;
 
 	public String label;
