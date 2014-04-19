a6bc40e304ceb7ceb4eab8270b5a5fca2ae78df9
NBlackout <NBlackout@hotmail.fr>
2014-04-19 18:15:24 +0200

    Random transaction generation for account

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import models.Account;
 import models.Bank;
+import models.Transaction;
 import play.mvc.Controller;
 
 public class Application extends Controller {
 
 	public static void index() {
+		Transaction.deleteAll();
 		Account.deleteAll();
 		Bank.deleteAll();
 
@@ -33,5 +35,32 @@ public class Application extends Controller {
 		List<Account> accounts = Account.findAll();
 
 		render(accounts);
+	}
+
+	public static void showAccount(long accountId) {
+		Account account = Account.findById(accountId);
+
+		if (Transaction.count("account.id", accountId) == 0) {
+			for (int i = 0; i < 50; i++) {
+				Calendar now = Calendar.getInstance();
+				now.add(Calendar.DATE, -i);
+
+				int perDay = (int) (Math.random() * 6);
+				for (int j = 0; j < perDay; j++) {
+					double amount = Math.random() * 300 - 200;
+
+					Transaction transaction = new Transaction();
+					transaction.account = account;
+					transaction.label = ((amount >= 0) ? "Débit" : "Crédit") + " " + (Transaction.count() + 1);
+					transaction.amount = amount;
+					transaction.date = now.getTimeInMillis();
+					transaction.save();
+				}
+			}
+		}
+
+		List<Transaction> transactions = Transaction.find("account.id = :accountId order by date desc").setParameter("accountId", accountId).fetch();
+
+		render(transactions);
 	}
 }
