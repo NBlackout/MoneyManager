61acce72bd9599286aaf78c038dc4b939238cacc
NBlackout <NBlackout@hotmail.fr>
2014-06-14 13:59:59 +0200

    Local time now displayed

--- app/models/Account.java
+++ app/models/Account.java
@@ -6,8 +6,10 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.Type;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 import play.db.jpa.Model;
+import play.i18n.Messages;
 
 @Entity
 @Table(name = "`Account`")
@@ -32,6 +34,20 @@ public class Account extends Model {
 
 	@Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
 	public DateTime lastSync;
+
+	public DateTime getLocalLastSync() {
+		DateTime localLastSync = null;
+
+		if (lastSync != null) {
+			String id = Messages.get("dates.timezone.id");
+			DateTimeZone local = DateTimeZone.forID(id);
+			long millis = local.convertUTCToLocal(lastSync.getMillis());
+
+			localLastSync = new DateTime(millis);
+		}
+
+		return localLastSync;
+	}
 
 	public String getFullNumber() {
 		StringBuilder builder = new StringBuilder();
